b318470df093fd26d60083d0d8fb3b3c4bf18cfc
Heidi Vanparys <hevan@sdfi.dk>
2022-12-22 15:07:05 +0100

    Tilføj indholdet fra bilag 3 af Vejledning om geografi i lovgivningen
    Ændringer i forhold til den oprindelige tekst:
    - tilføjelse af en indledende tekst med målgruppe, baggrund og
    læsevejledning
    - tilføjelse af noter ang. standarder som skal købes vs. kan findes
    online
    - opdatering af beskrivelse af INSPIRE-direktiv
    - rettelse af nogle (stave)fejl
    - opdatering af bestemte URL'er

---
+++ build/create_html.ps1
@@ -0,0 +1,27 @@
+# Windows PowerShell script to create the HTML file.
+# saxonJar is the location of the Saxon jar file
+param([Parameter(Mandatory=$true)] $saxonJar)
+
+# change to unicode
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+$folder = "..\target"
+if (Test-Path $folder) {
+	Remove-Item -Recurse $folder
+    Write-Host "Deleted folder " + $folder
+	New-Item $folder -ItemType Directory
+	Write-Host "Folder " + $folder + " created successfully"
+}
+
+$assetsFolder = $folder + "\assets"
+Copy-Item -Path "..\docs\assets" -Destination $assetsFolder -Recurse
+
+# retrieve today's date, for setting the revision date
+$today = Get-Date -Format "yyyy-MM-dd"
+
+Write-Host "Running asciidoctorj"
+$tmp_output = "index_tmp.html"
+asciidoctorj -b xhtml5 -a revdate=$today -a stylesheet! -o $tmp_output -D $folder ..\docs\index.adoc
+Write-Host "Running Saxon"
+java -jar $saxonJar -s:..\target\$tmp_output -xsl:prepare_html_for_styling.xsl -o:..\target\index.html
+Write-Host "Finished"
