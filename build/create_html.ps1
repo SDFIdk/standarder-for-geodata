192e7d523f5020859d19d8777afb95710e9f4c10
Heidi Vanparys <hevan@kds.dk>
2025-09-02 20:55:08 +0200

    Forbedr build script og sæt default værdier for parametrene

--- build/create_html.ps1
+++ build/create_html.ps1
@@ -1,39 +1,51 @@
 # Windows PowerShell script to create the HTML file.
 # asciidoctorj and Saxon (both Java applications) are used
-param([Parameter(Mandatory=$true)] $docVersion)
+param(
+    [Parameter(Mandatory=$false)] [string]$DocVersion = "udkast",
+    [Parameter(Mandatory=$false)] [switch]$SkipOpenHtml
+)
 
-# change to unicode
-[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+# change to UTF-8
+chcp 65001
 
-# Check whether SAXON_CP user environment variable is set. SAXON_CP has to point to the main jar file in the Saxon distribution, see https://www.saxonica.com.
-if ([System.Environment]::GetEnvironmentVariable("SAXON_CP", [System.EnvironmentVariableTarget]::User) -eq $null) {
-    Write-Error "The user environment variable SAXON_CP is not set. Script will exit."
-    exit
+# Check whether SAXON_CP environment variable is set. SAXON_CP has to point to the main jar file in the Saxon distribution, see https://www.saxonica.com.
+if (-not $env:SAXON_CP) {
+    Write-Error "The environment variable SAXON_CP is not set. Script will exit."
+    exit 1
 } else {
 	Write-Host "SAXON_CP=$env:SAXON_CP"
 }
 
-$folder = "target"
-if (Test-Path $folder) {
-	Remove-Item -Recurse $folder
-    Write-Host "Deleted folder $folder"
+$Folder = "target"
+if (Test-Path $Folder) {
+	Remove-Item -Recurse $Folder
+    Write-Host "Deleted folder $Folder"
 }
-New-Item $folder -ItemType Directory
-Write-Host "Folder $folder created successfully"
+New-Item $Folder -ItemType Directory
+Write-Host "Folder $Folder created successfully"
 
 # retrieve today's date, for setting the revision date
-$today = Get-Date -Format "yyyy-MM-dd"
+$Today = Get-Date -Format "yyyy-MM-dd"
 
 Write-Host "Create HTML page with asciidoctorj"
-$tmpOutput = "index_tmp.html"
-$finalOutput = "index.html"
-asciidoctorj -b xhtml5 -a revdate=$today -a revnumber=$docVersion -a stylesheet! -o $tmpOutput -D $folder docs/index.adoc
+$TmpOutputFileName = "index_tmp.html"
+$TmpOutput = Join-Path $Folder $TmpOutputFileName
+asciidoctorj -b xhtml5 -a revdate=$Today -a revnumber=$DocVersion -a stylesheet! -o $TmpOutputFileName -D $Folder docs/index.adoc
+
 Write-Host "Update HTML page with Saxon"
-$command = "$env:JAVA_HOME/bin/java.exe -cp $env:SAXON_CP net.sf.saxon.Transform -s:$folder/$tmpOutput -xsl:build/prepare_html_for_styling.xsl -o:$folder/$finalOutput"
-Invoke-Expression $command
-if (Test-Path "$folder/$finalOutput") {
-	Invoke-Item "$folder/$finalOutput"
-	Write-Host "HTML page $folder/$finalOutput created successfully"
+$FinalOutput = Join-Path $Folder "index.html"
+$Java = Join-Path $env:JAVA_HOME "bin/java.exe"
+& $Java `
+    -cp "$env:SAXON_CP" `
+    net.sf.saxon.Transform `
+    -s:"$TmpOutput" `
+    -xsl:"build/prepare_html_for_styling.xsl" `
+    -o:"$FinalOutput"
+if (Test-Path $FinalOutput) {
+	Write-Host "HTML page $FinalOutput created successfully"
+    if (-not $SkipOpenHtml) {
+		Invoke-Item $FinalOutput
+    }
 } else {
 	Write-Error "HTML page not created"
 }
